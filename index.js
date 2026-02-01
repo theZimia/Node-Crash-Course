2719869bba32ca41b6fff3dfda3731e806514469
Umme Salma Zimia <ummesalmazimia1@gmail.com>
2026-02-02 01:24:39 +0600

    classees

---
+++ index.js
@@ -0,0 +1,9 @@
+const Person = require('./person');
+
+const person1 = new Person;
+
+person1.name = 'Zimia';
+person1.age = 24;
+
+person1.greeting()
+
---
+++ person.js
@@ -0,0 +1,15 @@
+class Person{
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+
+    greeting(){
+        console.log(`My name is ${this.name}, i am ${this.age} years old`);
+    }
+
+    
+
+}
+
+module.exports = Person
