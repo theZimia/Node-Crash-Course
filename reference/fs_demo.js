bd1d80c46f42156c61d06a03f35db67f8119399a
Umme Salma Zimia <ummesalmazimia1@gmail.com>
2026-02-11 21:27:33 +0600

    os demo added

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -9,9 +9,40 @@ const path = require('path');
 
 //Create and write to file
 
-fs.writeFile(path.join(__dirname,"/test", "hello.txt"), "Hello World" , err => { 
-    if(err) throw err;
-    console.log("File Written to..");
+// fs.writeFile(path.join(__dirname,"/test", "hello.txt"), "Hello World" , err => { 
+//     if(err) throw err;
+//     console.log("File Written to..");
+//     // file append
+
+//     fs.appendFile(path.join(__dirname,"/test", "hello.txt"), "Hello World" , err => { 
+//     if(err) throw err;
+//     console.log("File Written to..");
+// });
+
+// });
+
+
+// // Read File
+// fs.readFile(path.join(__dirname,"/test") ,'utf8', (err, data) => { 
+//     if (err) {
+//     console.error(err);
+//     return;
+//   }
+//     console.log(data);
+    
+// });
+
+// Rename File
+fs.rename(
+    path.join(__dirname,"/test", "HelloWorld.txt") ,
+    path.join(__dirname,"/test", "Helloworld.txt"), 
+    err => { 
+        if(err) throw err;
+        console.log('Renamed');
+    
 });
+
+
+
 
 
